3bbdb6ac9ff52c61671e7cfef5e181e90560c398
garalapallil <vedasri@gmail.com~^c>
2023-04-22 19:09:32 +0530

    added comments in q5.c

--- q5.c
+++ q5.c
@@ -6,20 +6,20 @@ float marks;
 printf("\nenter the marks of the student:\n");
 scanf("%f",&marks);
 
-if(marks>=86&&marks<=100)
+if(marks>=86&&marks<=100)//if marks are between 86 to 100 student gets A+ grade.
 printf("\nA+\n");
-else if(marks>=71&&marks<86)
+else if(marks>=71&&marks<86)//if marks are between 71 to 86 student gets A- grade.
 printf("\nA-\n");
-else if(marks>=56&&marks<71)
+else if(marks>=56&&marks<71)//if marks are between 56 to 71 student gets B+ grade.
 printf("\nB+\n");
-else if(marks>=41&&marks<56)
+else if(marks>=41&&marks<56)//if marks are between 41 to 56 student gets B- grade.
 printf("\nB-\n");
-else if(marks>=26&&marks<41)
+else if(marks>=26&&marks<41)//if marks are between 26 to 41 student gets C grade.
 printf("\nC\n");
-else if(marks<26)
+else if(marks<26)//if marks are less than 26 student gets F grade.
 printf("\nF\n");
 else 
-printf("\ninvalid marks\n");
+printf("\ninvalid marks\n");//in all other cases 
 
 return 0;
 }
